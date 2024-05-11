b013bdd03ce578ff9ac527dab17eed8c43b94f2d
Fouad Aladhami <fouadiadhami@gmail.com>
2024-05-12 01:37:34 +0300

    data receiver file added and main updated

---
+++ data_receiver.py
@@ -0,0 +1,31 @@
+import serial
+import time
+
+# Serial port configuration
+serial_port = '/dev/cu.usbmodem12301'  # Change this to the appropriate port
+baud_rate = 9600
+
+# File path
+file_path = 'test_data1.txt'  # Change this path as needed
+
+try:
+    # Open the serial port
+    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+
+    # Open the file in write mode
+    with open(file_path, 'w') as file:
+        while True:
+            # Read data from serial port
+            data = ser.readline().decode().strip()  # Decode bytes to string and strip newline characters
+            print(data)
+            if data:
+                # Write data to the file
+                file.write(data + '\n')
+                file.flush()  # Ensure data is written immediately
+
+            time.sleep(0.005)  # Adjust sleep time as needed
+except Exception as e:
+    print("Error:", e)
+finally:
+    # Close the serial port
+    ser.close()
