08655274cce25bb4fa20d682f6760e6e330061d0
Fouad Aladhami <fouadiadhami@gmail.com>
2024-05-21 22:17:31 +0300

    annem yuruyor verileri eklendi

--- data/data_receiver.py
+++ data/data_receiver.py
@@ -6,7 +6,7 @@ serial_port = '/dev/cu.usbmodem12301'  # Change this to the appropriate port
 baud_rate = 9600
 
 # File path
-file_path = 'fuad_yuruyor_kosuyor_merdiven_cıkıyor.txt'  # Change this path as needed
+file_path = 'annem_yuruyor_ayak1.txt'  # Change this path as needed
 
 try:
     # Open the serial port
